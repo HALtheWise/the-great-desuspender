0b4fb5cb37df0b49e09f5aecbb6bdc1b6ede2e0a
Eric Miller <eric@skydio.com>
2021-02-04 16:38:35 -0800

    Desuspendy

---
+++ background.js
@@ -0,0 +1,22 @@
+function fix_tabs(){
+    chrome.tabs.query({}, function(tabs){
+        var num_fixed = 0
+        for (tab of tabs){
+            if (tab.url.startsWith("chrome-extension://klbibkeccnjlkjkiokjodocebajanakg")){
+                const substr = tab.url.slice(tab.url.indexOf("url=")+4);
+                console.log(substr);
+                num_fixed += 1;
+            }
+        }
+        alert("Scanned: " + tabs.length + ", Fixed: " + num_fixed)
+    })
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+fix_tabs();
+})
+
+chrome.runtime.onInstalled.addListener(function() {
+    console.log("Installed");
+    // fix_tabs();
+  });
